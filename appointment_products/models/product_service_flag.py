e5b55891b6e930164947e71f52812e913ba25230
MHD Manar Zamrik <manar.z@ft.iq>
2025-06-25 22:03:10 +0300

    Delete appointment_products directory

--- appointment_products/models/product_service_flag.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import fields, models
-
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    is_service_fsm = fields.Boolean(string='الخدمة', help='حدد إذا كان هذا المنتج يمثل خدمة ميدانية.') 
